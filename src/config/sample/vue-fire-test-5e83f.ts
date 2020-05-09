76fc21993dbb10f612021d12086c0b82c8bc265e
Masamichi Nakamura <masamichi.nakamura@gmail.com>
2020-05-09 23:00:34 +0900

    changed project auth data

---
+++ src/config/sample/vue-fire-test-5e83f.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyB1ed3g-BsfeTa7c1sfUz37GwmZHG7BnJ8",
+	authDomain: "vue-fire-test-5e83f.firebaseapp.com",
+	databaseURL: "https://vue-fire-test-5e83f.firebaseio.com",
+	projectId: "vue-fire-test-5e83f",
+	storageBucket: "vue-fire-test-5e83f.appspot.com",
+	messagingSenderId: "1060857299072",
+	appId: "1:1060857299072:web:e2390bc3099f7ad4d22665"
+};
